44fc05a06ac5ce7aa0192c41f6b3b23979e856e9
photovoltex <photovoltex@gmail.com>
2023-11-20 21:49:16 +0100

    maybe working listen

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,14 @@
+use std::time::SystemTime;
+
+use wasm_bindgen::closure::Closure;
+
+
+#[::wasm_bindgen::prelude::wasm_bindgen]
+extern "C" {
+    #[::wasm_bindgen::prelude::wasm_bindgen(js_namespace = ["window", "__TAURI__", "event"])]
+    fn listen(event: &str, closure: &::wasm_bindgen::prelude::Closure<dyn Fn(::wasm_bindgen::JsValue)>) -> ::js_sys::Function;
+}
+
 fn main() {
     // if the project is loaded, currently rust-analyzer seems to be 
     // unable to compile different workspace-members with different targets, 
@@ -7,5 +18,25 @@ fn main() {
     tauri_interop_api::cmd::empty_invoke();
     wasm_bindgen_futures::spawn_local(async {
         tauri_interop_api::cmd::invoke_promise_with_app_handle_as_argument().await.unwrap()
-    })
+    });
+
+    let closure = Closure::new(|_| println!("test"));
+    let unlisten = listen("event", &closure);
+
+    let now = SystemTime::now();
+
+    while let Ok(elapsed) = now.elapsed() {
+        if elapsed.as_millis() < 5000 {
+            break;
+        }
+    }
+
+    let _ = unlisten.call0(&Default::default());
+
+    let now = SystemTime::now();
+    while let Ok(elapsed) = now.elapsed() {
+        if elapsed.as_millis() < 5000 {
+            break;
+        }
+    }
 }
