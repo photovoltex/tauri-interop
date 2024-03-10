7d4fc369b02205d2e7f2e7bec948bb790a9b64b5
photovoltex <photovoltex@gmail.com>
2024-03-10 23:06:15 +0100

    add own docs for the main crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 #![warn(missing_docs)]
-#![doc = include_str!("../README.md")]
 #![feature(trait_alias)]
+
+//! Tauri-Interop is a library that provides macros to improve developing tauri apps with a rust 
+//! frontend by generating frontend implementation out of the backend definitions. 
+//! 
+//! The main macros intended to be used are:
+//! - [macro@command], which is intended to be used as replacement to [macro@tauri::command]
+//! - [macro@Event], that provides an easier usage of the [Events feature of tauri](https://tauri.app/v1/guides/features/events/)
+//!     - derives [event::Listen] when compiling to wasm and [event::Emit] otherwise
+//! 
+//! Additionally, some QOL macros ([host_usage] and [wasm_usage]) are provided that 
+//! reduce some drawbacks when simultaneously compiling to wasm and the host architecture.
+//! 
+//! ### Explanation and Examples
+//! 
+//! Detail explanations and example can be found on the respected traits or macros. Some 
+//! examples are ignored because they are only valid when compiling to wasm.
 
 pub use tauri_interop_macro::*;
 
