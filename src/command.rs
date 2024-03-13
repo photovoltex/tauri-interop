e625ed4c1ab535fb8ebbfdb82f4d6ee2a027f491
photovoltex <photovoltex@gmail.com>
2024-03-13 18:57:03 +0100

    remove _wasm feature with doc

--- src/command.rs
+++ src/command.rs
@@ -2,7 +2,7 @@
 pub use type_aliases::*;
 
 /// wasm bindings for tauri's provided js functions (target: `wasm`)
-#[cfg(any(target_family = "wasm", feature = "_wasm"))]
+#[cfg(any(target_family = "wasm", doc))]
 pub mod bindings;
 
 #[cfg(not(target_family = "wasm"))]
