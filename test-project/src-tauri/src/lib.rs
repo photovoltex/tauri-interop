a00add947f62335650094418d9e04df983eb6099
Felix Prillwitz <photovoltex@gmail.com>
2025-01-19 12:48:24 +0100

    upgrade to tauri 2

---
+++ test-project/src-tauri/src/lib.rs
@@ -0,0 +1,23 @@
+use std::sync::RwLock;
+use tauri::Manager;
+use api::model::TestState;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(api::get_all_handlers())
+        .setup(move |app| {
+            let main_window = app.handle().get_window("main").unwrap();
+
+            // debugging: always open dev tools on launch
+            main_window.open_devtools();
+
+            let test_state = RwLock::new(TestState::default());
+            app.manage(test_state);
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
--- test-project/src-tauri/src/main.rs
+++ test-project/src-tauri/src/main.rs
@@ -1,27 +1,8 @@
-use std::sync::RwLock;
-
-use api::model::TestState;
-use tauri::Manager;
-
 fn main() {
     env_logger::builder()
         .filter_level(log::LevelFilter::Trace)
         .try_init()
         .unwrap();
 
-    tauri::Builder::default()
-        .invoke_handler(api::get_all_handlers())
-        .setup(move |app| {
-            let main_window = app.handle().get_window("main").unwrap();
-
-            // debugging: always open dev tools on launch
-            main_window.open_devtools();
-
-            let test_state = RwLock::new(TestState::default());
-            app.manage(test_state);
-
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    src_tauri::run()
 }
